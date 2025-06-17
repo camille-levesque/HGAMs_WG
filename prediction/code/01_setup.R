1fe13e6a93e5ccbd7c882abea6546048d5b49512
camille-levesque <camille.levesque2@usherbrooke.ca>
2025-06-17 09:27:47 -0400

    Creation of R scripts

---
+++ prediction/code/01_setup.R
@@ -0,0 +1,17 @@
+# load packages
+
+library(mvgam)           # Fit, interrogate and forecast DGAMs
+library(dplyr)           # Tidy and flexible data manipulation
+library(ggplot2)         # Flexible plotting
+library(gratia)          # Graceful ggplot-based graphics for GAMs
+library(marginaleffects) # Compute interpretable model predictions
+library(tidybayes)       # Tidy manipulation / plots of posterior draws
+
+
+# load data
+
+# adjust data
+
+  # changer type colonne (as.factor, etc.)
+  # formatter
+  # creer nouvelles colonnes, etc.
