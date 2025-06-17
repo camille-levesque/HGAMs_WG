bb0829635b550d33bc0a43035fb65739c02b306c
camille-levesque <camille.levesque2@usherbrooke.ca>
2025-06-17 09:51:26 -0400

    Downloading & installing packages to be able to import data

--- prediction/code/01_setup.R
+++ prediction/code/01_setup.R
@@ -6,6 +6,23 @@ library(ggplot2)         # Flexible plotting
 library(gratia)          # Graceful ggplot-based graphics for GAMs
 library(marginaleffects) # Compute interpretable model predictions
 library(tidybayes)       # Tidy manipulation / plots of posterior draws
+library(tidyverse)
+
+
+## devtools: you need to install the package via the GitHub repository
+
+##install.packages("bbsBayes2", repos = c(bbsbayes = "https://bbsbayes.r-universe.dev",
+##                                        CRAN = getOption("repos")))
+library(bbsBayes2) # v1.1.2.1
+library(cmdstanr) # v0.7.1
+
+cmdstanr::install_cmdstan()
+
+cmdstanr::check_cmdstan_toolchain(fix = TRUE)
+
+fetch_bbs_data()
+
+
 
 
 # load data
@@ -15,3 +32,5 @@ library(tidybayes)       # Tidy manipulation / plots of posterior draws
   # changer type colonne (as.factor, etc.)
   # formatter
   # creer nouvelles colonnes, etc.
+
+#vignettes/articles/bbsBayes2.Rmd
