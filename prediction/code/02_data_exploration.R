1ea9531b417f27e9c475144fe24d26fb0c2f6182
camille-levesque <camille.levesque2@usherbrooke.ca>
2025-06-25 13:14:24 -0400

    Create 02_data_exploration.R

---
+++ prediction/code/02_data_exploration.R
@@ -0,0 +1,44 @@
+# Load previous scripts ---------------------------------------------------
+# source("code/01-set_up.R")
+# Data subsets ------------------------------------------------------------
+
+##########################################
+# Katherine Hebert's data exploration
+
+# summarise data contents
+dls = d_crop |>
+  group_by(valid_name) |>
+  group_split()
+
+# check for basic population trends
+mls = lapply(dls, function(x){lm(ABUNDANCE ~ YEAR, data = x)})
+
+# extract slopes
+coefs = mls |> lapply(coef) |> 
+  bind_rows() |> 
+  mutate("species" = unique(d_crop$valid_name))
+
+# plot to see the slopes
+ggplot(data = coefs) +
+  geom_point(aes(y = species, x = YEAR))
+#ggsave("variance/figures/linear_slopes.png")
+
+##########################################
+
+# Camille and Kim's data exploration (using Nick Clark's portal example)
+
+# Look at the variables' format
+dplyr::glimpse(d)
+  # We check the formats of the variables ; we do not need to change them.
+
+# NAs
+max(d$YEAR)
+image(is.na(t(d %>%
+                dplyr::arrange(dplyr::desc(YEAR)))), axes = F,
+      col = c('grey80', 'darkred'))
+axis(3, at = seq(0,1, len = NCOL(d)), 
+     labels = colnames(d))
+  # No NAs
+
+
+
