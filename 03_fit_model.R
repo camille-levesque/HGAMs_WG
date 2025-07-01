55077f4c7c5f919fd6b5118cb4b64fd962a31911
Kim Menard <kim.menard.2@ulaval.ca>
2025-07-01 11:01:39 -0400

    Delete the file to create a new one in the code folder of the prediction section

--- 03_fit_model.R
+++
@@ -1,4 +0,0 @@
-# Load previous scripts ---------------------------------------------------
-source("prediction/code/01_setup.R") # Import & format data and load libraries
-source("prediction/code/02_data_exploration") # Data exploration
-# Model fitting  --------------------------------------------------------
